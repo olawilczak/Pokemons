931c17f9e54fe58ca75fe5e807e31cf6e64eb800
olawilczak <a.wilczak95@gmail.com>
2022-08-26 10:06:14 +0200

    4

--- projekt/pokemons/src/App.js
+++ projekt/pokemons/src/App.js
@@ -5,7 +5,7 @@ import Login from './Login'
 import Registration from './Registration'
 import Edition from './Edition'
 import LogOut from './LogOut'
-import Pokemons from './Pokemons'
+
 import Search from './Search'
 
 
@@ -13,21 +13,19 @@ import Search from './Search'
 function App() {
   return (
     <div className="App">
-      <div className="App-head">
-      <div className="App-navigation">
-      <Logo/>
-      </div>
-      <div className="App-buttons">
-      <Favourities/>
-      <Login/>
-      <Registration/>
-      <Edition/>
-      <LogOut/>
-      </div>
-      </div>
+        <div className="App-navigation">
+        <Logo/>
+        </div>
+        <div className="App-buttons">
+        <Favourities/>
+        <Login/>
+        <Registration/>
+        <Edition/>
+        <LogOut/>
+        </div>
+      <div className="App-another">
       <Search/>
-      <Pokemons/>
-      
+      </div>
     </div>
   );
 }
