8d2a74a0fade2abc214fde9cedf670a56d6965a1
olawilczak <a.wilczak95@gmail.com>
2022-10-20 20:54:12 +0200

    pokemon

--- projekt/pokemons/src/Registration.js
+++ projekt/pokemons/src/Registration.js
@@ -1,104 +1,18 @@
-import {useFormik} from "formik";
-import { basicSchema } from "./schemas";
+import { useState } from "react";
 
 const Registration = (()=>{
 
-    const onSubmit = async (values, actions) => {
-        await new Promise((resolve)=> setTimeout(resolve, 1000));
-        actions.resetForm(); 
-        }
-        
-        const RegistrationFormula = ()=>{
-        const {
-            values,
-             errors,
-              touched,
-               isSubmitting,
-                handleBlur,
-                 handleChange,
-                  handleSubmit
-                } = useFormik({
-            initialValues: {
-                name: "",
-                email: "",
-                password: "",
-                confirmPassword: "",
-            },
-            validationSchema: basicSchema,
-            onSubmit,
-        });
-            return (
-            <div>
-             <button className="button"> Rejestracja
-             <div>
-              <form onSubmit={handleSubmit} autoComplete="off">
-                <label htmlFor="name">Name</label>
-                <input
-                value={values.name}
-                onChange={handleChange}
-                id="name"
-                type="name"
-                placeholder="Enter your name"
-                onBlur={handleBlur}
-                className={errors.name && touched.name ? "input-error" : ""}
-                
-                />
+    const[activePosition, setActivePosition]=useState()
+    
+    const handleClick = (event) => {
+        setActivePosition(event.target.innerHTML)
+    }
 
-                {errors.name && touched.name && (
-                    <p className="error">{errors.name}</p>
-                )}
-        
-                <label htmlFor="email">Email</label>
-                <input
-                value={values.email}
-                onChange={handleChange}
-                id="email"
-                type="email"
-                placeholder="Enter your email"
-                onBlur={handleBlur}
-                className={errors.email && touched.email ? "input-error" : ""}
-                
-                />
-            {errors.email && touched.email && (
-                    <p className="error">{errors.email}</p>
-                    )}
-        <label htmlFor="password">Password</label>
-                <input
-                id="password"
-                type="password"
-                placeholder="Enter your password"
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                className={errors.password && touched.password ? "input-error" : ""}
-               
-                />
-                {errors.password && touched.password && (
-                    <p className="error">{errors.password}</p>
-                    )}
-        <label htmlFor="confirmPassword">Confirm Password</label>
-                <input
-                id="confirmPassword"
-                type="password"
-                placeholder="Confirm password"
-                value={values.confirmPassword}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                className={errors.confirmPassword && touched.confirmPassword ? "input-error" : ""}
-                
-                />
-                {errors.confirmPassword && touched.confirmPassword && (
-                    <p className="error">{errors.confirmPassword}</p>
-                    )}
-              </form>
-              <button type="submit">Submit</button>
-              </div>
-              </button>
-              </div>
-            );
-        }
-
+    return ( 
+        <div>
+            <button onClick={handleClick} className="button">Rejestracja</button>
+        </div>
+    )
 });
-
 
 export default Registration;
